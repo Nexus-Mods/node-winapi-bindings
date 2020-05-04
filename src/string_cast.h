f73c24746f5cc6dc2913d6e53d2c3d36dd667b2f
TanninOne <seppleviathan@gmx.de>
2020-05-04 14:40:19 +0200

    handle case where converted multibyte stream is invalid

--- src/string_cast.h
+++ src/string_cast.h
@@ -70,7 +70,7 @@ static std::string toMB(const wchar_t * const &source, CodePage codePage, size_t
       throw std::runtime_error("string conversion failed");
     }
     // fix output string length (i.e. in case of unconvertible characters
-    while (result[outLength - 1] == L'\0') {
+    while ((outLength > 0) && (result[outLength - 1] == L'\0')) {
       result.resize(--outLength);
     }
   }
